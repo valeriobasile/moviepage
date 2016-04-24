fb5c4225598a2c52c1e5a0de27bf12620dc3e3d7
Valerio Basile <valeriobasile@gmail.com>
2016-04-24 12:27:58 +0200

    added search and sort

--- mymoviepage.py
+++ mymoviepage.py
@@ -14,6 +14,7 @@ ia = imdb.IMDb()
 file_ext = 'avi|divx|mkv|mpg|mp4|wmv|bin|ogm|vob|iso|img|bin|ts|rmvb|3gp|asf|flv|mov|movx|mpe|mpeg|mpg|mpv|ogg|ram|rm|wm|wmx|x264|xvid|dv|m4v'
 purge_words = 'divx|dvdscr|aac|dvdrip|brrip|UNRATED|WEBSCR|KLAXXON|xvid|r5|com--scOrp|300mbunited|1channel|3channel|bray|blueray|5channel|1GB|1080p|720p|480p|CD1|CD2|CD3|CD4|x264|x264-sUN|Special Edition|Sample|sample'
 CSS_FILE = 'mymoviepage.css'
+JS_FILE = 'list.js'
 
 def normalize_filename(movie_filename):
     file_ext_expr = "(?P<name>.*)\.({0})".format(file_ext)
@@ -89,32 +90,45 @@ def writehtmlheader(pagefile):
         f.write(u"""<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <link rel="stylesheet" type="text/css" href="mymoviepage.css" />
+        <link rel="stylesheet" type="text/css" href="{0}" />
         <link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css' />
         <link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css' />
         <meta http-equiv="content-type" content="text/php; charset=utf-8" />
         <title>MyMoviePage</title>
     </head>
-    <body>""".encode('utf-8'))
+    <body>
+        <div id="movies">
+        <div id="controls">
+          <input class="search" placeholder="Search" />
+          <button class="sort" data-sort="title">Sort by title</button>
+          <button class="sort" data-sort="genre">Sort by genre</button>
+          <button class="sort" data-sort="year">Sort by year</button>
+          <button class="sort" data-sort="rating">Sort by rating</button>
+        </div>
+          <ul class="list">""".format(CSS_FILE).encode('utf-8'))
 
 def writehtmlfooter(pagefile):
     with open(pagefile, 'a') as f:
-        f.write(u'</body>\n</html>\n'.encode('utf-8'))
+        f.write(u"""</ul>
+    </div>
+    <script src="{0}"></script>
+    </body>
+</html>""".format(JS_FILE).encode('utf-8'))
 
 def personlink(person):
     return u'<a href="http://www.imdb.com/name/nm{0}">{1}</a>'.format(person['id'], person['name'])
 
 def writehtmlentry(pagefile, movieinfo):
     with open(pagefile, 'a') as f:
-        f.write(u'<div class="movie">\n')
-        f.write(u'<h1><a href="http://www.imdb.com/title/tt{0}">{1}</a></h1>\n'.format(movieinfo['movieID'], movieinfo['title']).encode('utf-8'))
-        f.write(u'<h2>by {0} ({1})</h2>\n'.format(u', '.join(map(personlink, movieinfo['directors'])), movieinfo['year']).encode('utf-8'))
+        f.write(u'<li class="movie">\n')
+        f.write(u'<h1 class="title"><a href="http://www.imdb.com/title/tt{0}">{1}</a></h1>\n'.format(movieinfo['movieID'], movieinfo['title']).encode('utf-8'))
+        f.write(u'<h2>by {0} (<span class="year">{1}</span>)</h2>\n'.format(u', '.join(map(personlink, movieinfo['directors'])), movieinfo['year']).encode('utf-8'))
         f.write(u'<span class="genre">{0}</span>\n'.format(movieinfo['genre']).encode('utf-8'))
         f.write(u'<span class="plot">{0}</span>\n'.format(movieinfo['plot']).encode('utf-8'))
         f.write(u'<span class="cast">With {0}.</span>\n'.format(u', '.join(map(personlink, movieinfo['cast'][:4]))).encode('utf-8'))
         f.write(u'<span class="rating">IMDB Rating: {0}</span>\n'.format(movieinfo['rating']))
         #f.write(u'<pre>{0}, {1}</pre>\n'.format(movieinfo['movieID'], movieinfo['directorID']))
-        f.write(u'</div>\n')
+        f.write(u'</li>\n')
 
 def writehtmlpage(moviefiles, pagefile):
     writehtmlheader(pagefile)
@@ -191,8 +205,9 @@ if last_update_movies > last_update_pagefile or options.force:
 else:
     log.info('No movie found newer than the last update, nothing to do.')
 
-# copy CSS file
-try:
-    shutil.copy(CSS_FILE, target_dir)
-except:
-    log.error('Cannot copy style file {0} in {1}.'.format(CSS_FILE, target_dir))
+# copy static files
+for static_file in [CSS_FILE, JS_FILE]:
+    try:
+        shutil.copy(static_file, target_dir)
+    except:
+        log.error('Cannot copy style file {0} in {1}.'.format(static_file, target_dir))
